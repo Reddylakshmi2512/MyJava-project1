28a8d41a38d9993fe9917ccb252dea240f3d261e
reddy <reddylakshmi0777@gmail.com>
2026-09-21 10:20:10 +0000

    add-sub

---
+++ MyApp.java
@@ -0,0 +1,7 @@
+public class MyApp{
+	public static void main(String[] args){
+		int a=10;
+		int b=5;
+		System.out.println("Addition is:" ,+(a+b));
+		System.out.println("Difference is:",(a-b));
+	}
